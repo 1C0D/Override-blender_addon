ef2d4555260d2302dc629ce66a6a13e2835f1529
1C0D <63018905+1C0D@users.noreply.github.com>
2021-08-01 23:31:48 +0200

    ok this time working well

--- Override_v1_1.py
+++ Override_v1_1.py
@@ -5,39 +5,36 @@ import bpy
 bl_info = {
     "name": "OVERRIDE",
     "author": "1C0D",  # thks to devtools addon, 3di & dr Sybren from blender chat
-    "version": (1, 0, 0),
+    "version": (1, 1, 0),
     "blender": (2, 93, 0),
     "location": "texteditor/console",
     "description": "OVERRIDE SCRIPT and console excecution",
     "category": "Developpement",
 }
 
-#todo transform C. in context. and D. in data 
 
 console = 0
 active_text = None
 
+def expanse(line):
+    if "C." or "D." in line:
+        line = line.replace("C.", "bpy.context.").replace("D.", "bpy.data.")
+    return line
 
-def selectline_copy_suppr(context): # get text in console
+def printWrap(line):
+    line=f"print({line})"
+    return line
+
+def selectline_copy_suppr(self,context): # get text in console
     # selectline
     sc = context.space_data
     line_object = sc.history[-1]
     if line_object:
+        global line
         line = line_object.body
-        lenght = len(line)
-        st = sc.select_start = 0
-        se = sc.select_end = lenght
-    # copy
-    bpy.ops.console.copy()  # to paste in the text editor
-    # suppr
-    bpy.ops.console.move(type='LINE_END')
-    for _ in range(st):
-        bpy.ops.console.move(type='PREVIOUS_CHARACTER')
-    for _ in range(se-st):
-        bpy.ops.console.delete(type='PREVIOUS_CHARACTER')
-    sc.select_start = st
-    sc.select_end = st
-
+        line = expanse(line)
+        if self.print:
+            line = printWrap(line)
 
 def del_temp_text():
     if "temp_text" in bpy.data.texts.keys():
@@ -48,16 +45,18 @@ def create_text_active(context):
     global active_text
     global console
 
-    area = get_area(context, 'TEXT_EDITOR')
+    param = get_area(context, 'TEXT_EDITOR')
+    area=param[2]
     active_text = area.spaces[0].text
     del_temp_text() # clean up
     bpy.data.texts.new("temp_text") # create a text
     text = bpy.data.texts['temp_text']
     area.spaces[0].text = text # make it active
 
-    new_context = override(context, area)
+    new_context = override(context, *param)
     bpy.ops.text.insert(new_context, text='import bpy\n\n') #needed in text editor
-    bpy.ops.text.paste(new_context)
+    bpy.ops.text.insert(new_context, text=line)
+
     console = 1 #set the operator below to 1
     bpy.ops.text.override(new_context, "INVOKE_DEFAULT")
 
@@ -68,11 +67,14 @@ def get_area(context, area_type):
         for area in screen.areas:
             if area.type != area_type:
                 continue
-            return area
+            for region in area.regions:
+                if region.type != 'WINDOW':
+                    continue
+                return window, screen, area, region
 
 
-def override(context, area):
-    override = {'area': area}
+def override(context, *param):
+    override = {'window': param[0], 'screen': param[1], 'area': param[2], 'region': param[3]}
     return {
         **context.copy(),
         **override,
@@ -81,23 +83,29 @@ def override(context, area):
 
 def clean_temp_text(area):
     del_temp_text()
-    area.spaces[0].text = active_text
-
-
-def console_result(context, new_context): #to generate a result message in console
-    clip = context.window_manager.clipboard
-    text = f"' {clip} '"
-    bpy.ops.console.insert(new_context, text=text)
-    bpy.ops.console.execute(new_context)
+    if console:
+        area.spaces[0].text = active_text
 
 
 class OVERRIDE_OT_console(bpy.types.Operator): 
-    """Tooltip"""
+    """Override context clicking on target area after this button
+    you can wrap your command with a print pressing button+shift
+        
+    """
     bl_idname = "console.override"
     bl_label = "override console exe"
+    bl_option = {'INTERNAL'}
+
+    print : bpy.props.BoolProperty(default=False)
+
+    def invoke(self, context, event):
+        if event.shift:
+            self.print = True
+
+        return self.execute(context)
 
     def execute(self, context):
-        selectline_copy_suppr(context)
+        selectline_copy_suppr(self, context)
         create_text_active(context)
 
         return {'FINISHED'}
@@ -107,8 +115,9 @@ class OVERRIDE_OT_text_editor(bpy.types.Operator): #run for console and text edi
     """Tooltip"""
     bl_idname = "text.override"
     bl_label = "override text editor script"
+    bl_option = {'INTERNAL'}
 
-    def invoke(self, context, event):
+    def invoke(self, context, event):            
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
@@ -121,36 +130,40 @@ class OVERRIDE_OT_text_editor(bpy.types.Operator): #run for console and text edi
         screen = context.window.screen
         for area in screen.areas:
             if (area.x < event.mouse_x < area.x + area.width and area.y < event.mouse_y < area.y + area.height):
-                new_context = override(context, area)
-                try:
-                    bpy.ops.text.run_script(new_context)
-                except RuntimeError:
-                    if console:
-                        area = get_area(context, 'TEXT_EDITOR')
-                        clean_temp_text(area)
-                    self.report({'ERROR'}, "CONTEXT INCORRECT")
+                for region in area.regions:
+                    if region.type != 'WINDOW':
+                        continue
+                    param=(window, screen, area, region)
+                    new_context = override(context, *param)
+                    try:
+                        bpy.ops.text.run_script(new_context)
+                    except RuntimeError:
+                        if console:
+                            param = get_area(context, 'TEXT_EDITOR')
+                            clean_temp_text(param[2])
+                        self.report({'ERROR'}, "CONTEXT INCORRECT")
 
-                    return {'CANCELLED'}
+                        return {'CANCELLED'}                
 
         if console:
-            area = get_area(context, 'TEXT_EDITOR')
-            clean_temp_text(area)
+            param = get_area(context, 'TEXT_EDITOR')            
+            clean_temp_text(param[2])
 
-            area = get_area(context, 'CONSOLE')
-            new_context = override(context, area)
-            console_result(context, new_context)
+            param = get_area(context, 'CONSOLE')
+            new_context = override(context, *param)
+            self.report({'INFO'}, "EXECUTED! Check OS Console")
 
         return {'FINISHED'}
 
 
 def draw(self, context):
     layout = self.layout
-    layout.operator("text.override", text='', icon="CONSOLE")
+    layout.operator("text.override", text='', icon="DECORATE_DRIVER")
 
 
 def draw1(self, context):
     layout = self.layout
-    layout.operator("console.override", text='', icon="CONSOLE")
+    layout.operator("console.override", text='', icon="DECORATE_DRIVER")
 
 
 def register():
